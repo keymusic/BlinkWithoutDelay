14a2541b443a4c1080bf44cc85b2876e65a5f8c8
Michael Kotthaus <michael.kotthaus@gmx.net>
2019-05-05 23:48:00 +0200

    timing algorithm changed so that any delay in the scheduled action is not accumulated but compensated; blink frequency increased by the factor 4

--- BlinkWithoutDelay.ino
+++ BlinkWithoutDelay.ino
@@ -36,10 +36,10 @@ int ledState = LOW;             // ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long lastScheduledMillis = 0; // stores scheduled time, when the last action was performed
 
 // constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
+const long interval = 250;           // interval at which to blink (milliseconds)
 
 void setup() {
   // set the digital pin as output:
@@ -54,9 +54,9 @@ void loop() {
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+  if (currentMillis - lastScheduledMillis >= interval) {
+    // 2019-05-05: prevent time slippage
+    lastScheduledMillis += interval;
 
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
